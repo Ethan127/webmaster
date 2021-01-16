eaa70785e2246394e8b6bb5374a52f1715f3607d
Ethan127 <ethan.s127@gmail.com>
2021-01-16 16:22:39 -0700

    a

--- designbrief/n.js
+++ designbrief/n.js
@@ -23,3 +23,22 @@ var otherNav=function(){
 }
 
 var name = "Gomperta";
+
+var navbar = `
+<nav id="nav" class="navbar navbar-expand-lg fixed-top">
+    <div class="container"><a href="index.html" class="navbar-brand text-uppercase font-weight-bold"><img class="logo" src="../assets/logo.png" height="60px"></a>
+        
+        <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
+        
+        <div id="navbarSupportedContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href="index.html" class="nav-link font-weight-bold">Home</a></li>
+                <li class="nav-item"><a href="about.html" class="nav-link font-weight-bold">About Us</a></li>
+                <li class="nav-item"><a href="services.html" class="nav-link font-weight-bold">Services</a></li>
+                <li class="nav-item"><a href="contact.html" class="nav-link font-weight-bold">Contact</a></li>
+                <li class="nav-item"><a href="../index.html" class="nav-link font-weight-bold">Main Website</a></li>
+            </ul>
+        </div>
+
+    </div>
+</nav>`;
